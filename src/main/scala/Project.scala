8c919b43c68d210a24238f7e03367b71df3f4242
Preston <pgior001@ucr.edu>
2018-05-31 23:34:22 -0700

    query 1, start of query 2

--- src/main/scala/Project.scala
+++ src/main/scala/Project.scala
@@ -19,22 +19,36 @@ object Project {
   }
 
   private def part1(simba: SimbaSession): Unit = {
-    var ds = simba.read.option("inferSchema", "true").csv("/home/pgiorgianni/Downloads/trajectories.csv")
+    var ds = simba.read.option("inferSchema", "true").csv("/home/pgiorgianni/Downloads/trajectories.csv").cache()
     ds = ds.withColumnRenamed("_c0", "trajectoryIdentification")
     ds = ds.withColumnRenamed("_c1", "objectIdentification")
     ds = ds.withColumnRenamed("_c2", "longitude")
     ds = ds.withColumnRenamed("_c3", "latitude")
     ds = ds.withColumnRenamed("_c4", "timeRead")
 
+    var ds2 = simba.read.option("inferSchema", "true").csv("/home/pgiorgianni/Downloads/POIs.csv").cache()
+
+    ds2 = ds2.withColumnRenamed("_c0", "objectIdentification")
+    ds2 = ds2.withColumnRenamed("_c1", "description")
+    ds2 = ds2.withColumnRenamed("_c2", "longitude")
+    ds2 = ds2.withColumnRenamed("_c3", "latitude")
+
     println("---------------------------------------------------------")
     ds.printSchema()
-//    ds.createOrReplaceTempView("trajectory")
-//
+    ds2.printSchema()
+    ds2.show(20)
+
+    ds.createOrReplaceTempView("trajectory")
+
+    ds2.createOrReplaceTempView("poi")
+
 //    simba.indexTable("trajectory", RTreeType, "testtree",  Array("longitude", "latitude") )
-//
+//    simba.indexTable("poi", RTreeType, "poisIndex",  Array("longitude", "latitude") )
+//    simba.loadIndex("poisIndex", "/home/pgiorgianni/Downloads/POIsIndex")
+//    simba.persistIndex("poisIndex", "/home/pgiorgianni/Downloads/POIsIndex")
+
     import simba.simbaImplicits._
-    ds.range(Array("longitude", "latitude"),Array(-339220.0,  4444725),Array(-309375.0, 4478070.0)).show()
-    ds.circleRange(Array("longitude", "latitude"), Array(-322357.0, 4463408.0), 0.018018018).show()
-//    simba.showIndex("trajectory")
+    ds2.range(Array("longitude", "latitude"),Array(-339220.0,  4444725),Array(-309375.0, 4478070.0)).where("description LIKE \"amenity=restaurant\"").show()
+    ds.filter("date_format(timeRead, 'u') < 5").circleRange(Array("longitude", "latitude"), Array(-322357.0, 4463408.0), 1802.53).show()
   }
 }
