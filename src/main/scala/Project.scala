c5c1f549c20f4f8e47136812a156efb72691dba9
Preston <pgior001@ucr.edu>
2018-06-03 17:51:30 -0700

    questions 1-3 working

--- src/main/scala/Project.scala
+++ src/main/scala/Project.scala
@@ -2,7 +2,7 @@ import org.apache.spark.sql.simba.SimbaSession
 import org.apache.spark.sql.simba.index.RTreeType
 
 object Project {
-  case class PointData(trajectoryIdentification: String, objectIdentification: String, longitude: Double, latitude: Double, time: String)
+  case class PointData(trajectoryIdentification: String, objectIdentification: String, x: Double, y: Double, time: String)
 
   def main(args: Array[String]): Unit = {
 
@@ -14,46 +14,83 @@ object Project {
       .getOrCreate()
 
     part1(simbaSession)
+//    question1(simbaSession)
+//    question2(simbaSession)
+    question3(simbaSession)
     simbaSession.stop()
     simbaSession.close()
   }
 
   private def part1(simba: SimbaSession): Unit = {
-    var ds = simba.read.option("inferSchema", "true").csv("/home/pgiorgianni/Downloads/trajectories.csv").cache()
+    var ds = simba.read.option("inferSchema", "true").csv("/home/pgiorgianni/Downloads/trajectories.csv").cache
     ds = ds.withColumnRenamed("_c0", "trajectoryIdentification")
     ds = ds.withColumnRenamed("_c1", "objectIdentification")
-    ds = ds.withColumnRenamed("_c2", "longitude")
-    ds = ds.withColumnRenamed("_c3", "latitude")
+    ds = ds.withColumnRenamed("_c2", "x")
+    ds = ds.withColumnRenamed("_c3", "y")
     ds = ds.withColumnRenamed("_c4", "timeRead")
 
-    var ds2 = simba.read.option("inferSchema", "true").csv("/home/pgiorgianni/Downloads/POIs.csv").cache()
+    var ds2 = simba.read.option("inferSchema", "true").csv("/home/pgiorgianni/Downloads/POIs.csv").cache
 
     ds2 = ds2.withColumnRenamed("_c0", "objectIdentification")
     ds2 = ds2.withColumnRenamed("_c1", "description")
-    ds2 = ds2.withColumnRenamed("_c2", "longitude")
-    ds2 = ds2.withColumnRenamed("_c3", "latitude")
+    ds2 = ds2.withColumnRenamed("_c2", "x")
+    ds2 = ds2.withColumnRenamed("_c3", "y")
 
     println("---------------------------------------------------------")
     ds.printSchema()
     ds2.printSchema()
-    ds2.show(20)
 
     ds.createOrReplaceTempView("trajectory")
 
     ds2.createOrReplaceTempView("poi")
 
-//    simba.indexTable("trajectory", RTreeType, "testtree",  Array("longitude", "latitude") )
-    simba.indexTable("poi", RTreeType, "poisIndex",  Array("longitude", "latitude") )
-    simba.loadIndex("poisIndex", "/home/pgiorgianni/Downloads/POIsIndex")
+//    simba.indexTable("trajectory", RTreeType, "testtree",  Array("x", "y") )
+//    simba.indexTable("poi", RTreeType, "poisIndex",  Array("x", "y") )
+//    simba.loadIndex("poisIndex", "/home/pgiorgianni/Downloads/POIsIndex")
 //    simba.persistIndex("poisIndex", "/home/pgiorgianni/Downloads/POIsIndex")
-
-    import simba.simbaImplicits._
-    ds2.range(Array("longitude", "latitude"),Array(-339220.0,  4444725),Array(-309375.0, 4478070.0)).where("description LIKE \"amenity=restaurant\"").show()
-//    ds2.sqlContext.sql("SELECT * FROM trajectory WHERE POINT(longitude, latitude) IN RANGE(POINT(-339220.0,  4444725), POINT(-309375.0, 4478070.0))").show()
-    //http://epsg.io/4799 shows that the unit of this system is a meter. so is should be good like this
-    var tmp = ds.filter("date_format(timeRead, 'u') == 5").circleRange(Array("longitude", "latitude"), Array(-322357.0, 4463408.0), 2000)
-    tmp.show()
-//    tmp.select("objectIdentification", "timeRead").groupBy("objectIdentification")
-    println("------------------------------------------------------------------")
   }
+
+  private def question1 (simba: SimbaSession): Unit = {
+    import simba.simbaImplicits._
+        simba.sql("Select * from trajectory").range(Array("x", "y"),Array(-339220.0,  4444725),Array(-309375.0, 4478070.0))
+          .where("description LIKE \"amenity=restaurant\"").show()
+  }
+
+  private def question2 (simba: SimbaSession): Unit = {
+    import simba.simbaImplicits._
+    var tmp = simba.sql("Select objectIdentification, x, y, date_format(timeRead, 'u') as dow, date_format(timeRead, 'y') as Year," +
+      " date_format(timeRead, 'w') as week, date_format(timeRead, 'H') as hour from trajectory where date_format(timeRead, 'u') <= 5")
+      .circleRange(Array("x", "y"), Array(-322357.0, 4463408.0), 2000).select("objectIdentification", "dow",
+      "Year", "week", "hour").distinct().cache()
+    tmp.show(300)
+    var count = tmp.count()
+    var days = tmp.select("dow", "Year", "week", "hour").distinct().count()
+    println("----------------------------- Visitors A Day -------------------------------")
+    println(count)
+    println(days)
+    println(count.asInstanceOf[Double]/(days * 24))
+//    simba.sql("Select MAX(timeRead) as max, MIN(timeRead) as min from trajectory").show()
+  }
+
+  private def question3 (simba:SimbaSession): Unit = {
+    // -324297.5, 4461397.5
+    // upper left  (-339220, 4461397.5)      (-324297.5, 4478070)
+    // upper right (-324297.5, 4461397.5)    (-309375, 4478070)
+    // lower left  (-339220, 4444725)    (-324297.5, 4461397.5)
+    // lower right (-324297.5, 4444725)     (-309375, 4478070)
+    import simba.simbaImplicits._
+    var tmp = simba.sql("Select tt.* from (Select min(timeRead) as min, max(timeRead) as max, trajectoryIdentification from trajectory as t Group By trajectoryIdentification) as t" +
+      " Inner Join trajectory as tt on t.trajectoryIdentification = tt.trajectoryIdentification and (tt.timeRead = t.max or tt.timeRead = t.min)")
+      .range(Array("x", "y"),Array(-339220.0,  4444725),Array(-309375.0, 4478070.0))
+    tmp.show()
+    var totalPoints = tmp.groupBy("trajectoryIdentification").count().where("count = 2").count()
+    var selected = tmp.range(Array("x", "y"),Array(-339220, 4461397.5) ,Array(-324297.5, 4478070)).groupBy("trajectoryIdentification").count().where("count = 2").count()
+    selected += tmp.range(Array("x", "y"),Array(-324297.5, 4461397.5) ,Array(-309375.0, 4478070)).groupBy("trajectoryIdentification").count().where("count = 2").count()
+    selected += tmp.range(Array("x", "y"),Array(-339220.0, 4444725) ,Array(-324297.5, 4461397.5)).groupBy("trajectoryIdentification").count().where("count = 2").count()
+    selected += tmp.range(Array("x", "y"),Array(-339220, 4461397.5) ,Array(-324297.5, 4478070)).groupBy("trajectoryIdentification").count().where("count = 2").count()
+    println("total: " + totalPoints)
+    println("end in same: " + (totalPoints - (selected - totalPoints)))
+    println("end in different: " + (selected - totalPoints))
+  }
+
 }
