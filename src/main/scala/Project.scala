f37aef8bc0460d918d42346dd19e283bed8b0851
Preston <pgior001@ucr.edu>
2018-06-06 10:13:58 -0700

    number 5

--- src/main/scala/Project.scala
+++ src/main/scala/Project.scala
@@ -1,5 +1,8 @@
 import org.apache.spark.sql.simba.SimbaSession
 import org.apache.spark.sql.simba.index.RTreeType
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.Row
+
 
 object Project {
   case class PointData(trajectoryIdentification: String, objectIdentification: String, x: Double, y: Double, time: String)
@@ -17,27 +20,33 @@ object Project {
 //    question1(simbaSession)
 //    question2(simbaSession)
 //    question3(simbaSession)
-    question5(simbaSession)
+//    question4(simbaSession)
+//    question5(simbaSession)
     simbaSession.stop()
     simbaSession.close()
   }
 
+  private def listPartitions(iter: Iterator[Row]): Iterator[Row] = {
+    println(iter.toList)
+    return iter
+  }
+  case class Data(trajectoryIdentification: Int, objectIdentification: Int, x: Double, y: Double, timeRead: String)
+
   private def part1(simba: SimbaSession): Unit = {
-    var ds = simba.read.option("inferSchema", "true").csv("/home/kjkamgar/Downloads/trajectories.csv").cache
+    var ds = simba.read.option("inferSchema", "true").csv("/home/pgiorgianni/Downloads/trajectories.csv").cache
     ds = ds.withColumnRenamed("_c0", "trajectoryIdentification")
     ds = ds.withColumnRenamed("_c1", "objectIdentification")
     ds = ds.withColumnRenamed("_c2", "x")
     ds = ds.withColumnRenamed("_c3", "y")
     ds = ds.withColumnRenamed("_c4", "timeRead")
 
-    var ds2 = simba.read.option("inferSchema", "true").csv("/home/kjkamgar/Downloads/POIs.csv").cache
+    var ds2 = simba.read.option("inferSchema", "true").csv("/home/pgiorgianni/Downloads/POIs.csv").cache
 
     ds2 = ds2.withColumnRenamed("_c0", "objectIdentification")
     ds2 = ds2.withColumnRenamed("_c1", "description")
     ds2 = ds2.withColumnRenamed("_c2", "x")
     ds2 = ds2.withColumnRenamed("_c3", "y")
 
-    println("---------------------------------------------------------")
     ds.printSchema()
     ds2.printSchema()
 
@@ -45,29 +54,36 @@ object Project {
 
     ds2.createOrReplaceTempView("poi")
 
-//    simba.indexTable("trajectory", RTreeType, "testtree",  Array("x", "y"))
+//    simba.indexTable("trajectory", RTreeType, "trajectoriesIndex",  Array("x", "y"))
+//    simba.showIndex("trajectory")
+//    simba.sql("SELECT INDEX trajectoriesIndex")
 //    simba.indexTable("poi", RTreeType, "poisIndex",  Array("x", "y") )
-//    simba.loadIndex("poisIndex", "/home/pgiorgianni/Downloads/POIsIndex")
+//    simba.sql("SELECT INDEX poisIndex")
+
+    //    simba.loadIndex("poisIndex", "/home/pgiorgianni/Downloads/POIsIndex")
 //    simba.persistIndex("poisIndex", "/home/pgiorgianni/Downloads/POIsIndex")
   }
 
   private def question1 (simba: SimbaSession): Unit = {
     import simba.simbaImplicits._
-        simba.sql("Select * from trajectory").range(Array("x", "y"),Array(-339220.0,  4444725),Array(-309375.0, 4478070.0))
-          .where("description LIKE \"amenity=restaurant\"").show()
+    //just a simple range suery on the trajectory with a like filter on description to find the restaurants
+    simba.sql("Select * from poi").range(Array("x", "y"),Array(-339220.0,  4444725),Array(-309375.0, 4478070.0))
+      .where("description LIKE \"%restaurant%\"").show()
   }
 
   private def question2 (simba: SimbaSession): Unit = {
     import simba.simbaImplicits._
+    //use sql to select the point and break the date into fields of day of week, week of year, hour of day, and year. then do a range query on them.
     var tmp = simba.sql("Select objectIdentification, x, y, date_format(timeRead, 'u') as dow, date_format(timeRead, 'y') as Year," +
       " date_format(timeRead, 'w') as week, date_format(timeRead, 'H') as hour from trajectory where date_format(timeRead, 'u') <= 5")
       .circleRange(Array("x", "y"), Array(-322357.0, 4463408.0), 2000).select("objectIdentification", "dow",
       "Year", "week", "hour").distinct().cache()
-    tmp.show(300)
+    tmp.show(20)
+//    simba.sql("SELECT max(timeRead), min(timeRead) from trajectory where where date_format(timeRead, 'u') <= 5").show()
     var count = tmp.count()
-    var days = tmp.select("dow", "Year", "week", "hour").distinct().count()
-    println("----------------------------- Visitors A Day -------------------------------")
-    println("people found in the area: " + count)
+    //sort by unique hour and object to see how many objects are in the data
+    var days = tmp.select("dow", "Year", "week").distinct().count()
+    //output how many people were in the square divided by number of days * 24 hours that people were in the area
     println("days tracked: " + days)
     println("people/hours: " + count.asInstanceOf[Double]/(days * 24))
   }
@@ -79,16 +95,20 @@ object Project {
     // lower left  (-339220, 4444725)    (-324297.5, 4461397.5)
     // lower right (-324297.5, 4444725)     (-309375, 4478070)
     import simba.simbaImplicits._
+    //selects all points by the max and min times and then does a range query to get the area we are working it
     var tmp = simba.sql("Select tt.* from (Select min(timeRead) as min, max(timeRead) as max, trajectoryIdentification from trajectory as t Group By trajectoryIdentification) as t" +
       " Inner Join trajectory as tt on t.trajectoryIdentification = tt.trajectoryIdentification and (tt.timeRead = t.max or tt.timeRead = t.min)")
       .range(Array("x", "y"),Array(-339220.0,  4444725),Array(-309375.0, 4478070.0))
     tmp.show()
     //he would prefer us to take the centroids of the involved points.
+    //counts the number of points that have start and end in the range
     var totalPoints = tmp.groupBy("trajectoryIdentification").count().where("count = 2").count()
+    //counts the number of points in each rectangle
     var selected = tmp.range(Array("x", "y"),Array(-339220, 4461397.5) ,Array(-324297.5, 4478070)).groupBy("trajectoryIdentification").count().where("count = 2").count()
     selected += tmp.range(Array("x", "y"),Array(-324297.5, 4461397.5) ,Array(-309375.0, 4478070)).groupBy("trajectoryIdentification").count().where("count = 2").count()
     selected += tmp.range(Array("x", "y"),Array(-339220.0, 4444725) ,Array(-324297.5, 4461397.5)).groupBy("trajectoryIdentification").count().where("count = 2").count()
     selected += tmp.range(Array("x", "y"),Array(-339220, 4461397.5) ,Array(-324297.5, 4478070)).groupBy("trajectoryIdentification").count().where("count = 2").count()
+    //outputs the total number of points that start and end in the same range, start and end in a different range, and total
     println("total: " + totalPoints)
     println("end in same: " + (totalPoints - (selected - totalPoints)))
     println("end in different: " + (selected - totalPoints))
@@ -96,16 +116,19 @@ object Project {
 
   def question4(ds : SimbaSession) : Unit ={
     import ds.simbaImplicits._
-    var df = ds.sql("Select x,y from trajectories where date_format(timeRead,'m') <=  6 and date_format(timeread,'m') >= 2")
+    var df = ds.sql("Select x,y from trajectory where date_format(timeRead,'m') <=  6 and date_format(timeRead,'m') >= 2")
     var df2 = df.distinct()
-    df2.distanceJoin(df,Array("x","y"),Array("x","y"),20.0).groupBy("x","y").count().sort("count").show()
+    df2.distanceJoin(df,Array("x","y"),Array("x","y"),20.0).groupBy("x","y").count().sort(desc("count")).limit(20).show()
   }
 
   def question5(simba : SimbaSession) : Unit ={
     import simba.simbaImplicits._
+    //gets the values of each dataset and does some filtering for weekdays
     var poi = simba.sql("Select x as poix, y as poiy from poi")
     var trajectories = simba.sql("Select * from trajectory where where date_format(timeRead, 'u') <= 5")
-    poi.distanceJoin(trajectories, Array("poix", "poiy"), Array("x", "y"), 100).groupBy("poix", "poiy").count().orderBy("count").show()
+    //joins the sets where they are x distance appart then groups them by point and sorts them to count
+    var solution = poi.distanceJoin(trajectories, Array("poix", "poiy"), Array("x", "y"), 100).groupBy("poix", "poiy").count().sort(desc("count")).limit(20)
+    solution.show(20)
   }
 
 }
