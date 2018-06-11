6efd6d717165eb340a36ff49350422e1f7ae2b30
Preston <pgior001@ucr.edu>
2018-06-10 19:17:18 -0700

    finish 5 and mbr extraction code

--- src/main/scala/Project.scala
+++ src/main/scala/Project.scala
@@ -1,7 +1,12 @@
 import org.apache.spark.sql.simba.SimbaSession
 import org.apache.spark.sql.simba.index.RTreeType
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.Row
+import org.apache.spark.sql.{Column, Row}
+
+import scala.reflect.ClassTag
+import java.sql.Timestamp
+
+import scala.collection.mutable.ListBuffer
 
 
 object Project {
@@ -26,39 +31,86 @@ object Project {
     simbaSession.close()
   }
 
-  private def listPartitions(iter: Iterator[Row]): Iterator[Row] = {
-    println(iter.toList)
-    return iter
+  case class Data(trajectoryIdentification: Int, objectIdentification: Int, x: Double, y: Double, timeRead: Timestamp)
+  case class mbr(minX: Double, maxX: Double, minY: Double, maxY: Double)
+  implicit def kryoEncoder[A](implicit ct: ClassTag[A]) =
+    org.apache.spark.sql.Encoders.kryo[A](ct)
+
+
+  case class mbrs (mbrList: ListBuffer[mbr]) {
+    def addMbr(input: Iterator[Row]): mbrs = {
+      var init = false
+      var maxX = 0.0
+      var minX = 0.0
+      var maxY = 0.0
+      var minY = 0.0
+
+      input.foreach { elem =>
+        if(!init) {
+          maxX = elem.get(2).asInstanceOf[Double]
+          minX = elem.get(2).asInstanceOf[Double]
+          maxY = elem.get(3).asInstanceOf[Double]
+          minY = elem.get(3).asInstanceOf[Double]
+          init = true
+        }
+        var x = elem.get(2).asInstanceOf[Double]
+        var y = elem.get(3).asInstanceOf[Double]
+
+        if (x > maxX)
+          maxX = x
+        if(x <=minX)
+          minX = x
+        if(y > maxY)
+          maxY = y
+        if(y < minY)
+          minY = y
+
+      }
+//      println( mbr(minX, maxX, minY, maxY).toString )
+      mbrList += mbr(minX, maxX, minY, maxY)
+      this
+    }
+
+    def merge(other: mbrs): mbrs ={
+      other.mbrList.foreach(mbr => {
+        mbrList += mbr
+      })
+      this
+    }
+
   }
-  case class Data(trajectoryIdentification: Int, objectIdentification: Int, x: Double, y: Double, timeRead: String)
+
 
   private def part1(simba: SimbaSession): Unit = {
-    var ds = simba.read.option("inferSchema", "true").csv("/home/pgiorgianni/Downloads/tmp").cache
+    import simba.simbaImplicits._
+    var ds = simba.read.option("inferSchema", "true").csv("/home/pgiorgianni/Downloads/trajectories.csv")
     ds = ds.withColumnRenamed("_c0", "trajectoryIdentification")
     ds = ds.withColumnRenamed("_c1", "objectIdentification")
     ds = ds.withColumnRenamed("_c2", "x")
     ds = ds.withColumnRenamed("_c3", "y")
     ds = ds.withColumnRenamed("_c4", "timeRead")
 
-    var ds2 = simba.read.option("inferSchema", "true").csv("/home/pgiorgianni/Downloads/POIs.csv").cache
+    var ds2 = simba.read.option("inferSchema", "true").csv("/home/pgiorgianni/Downloads/POIs.csv")
 
     ds2 = ds2.withColumnRenamed("_c0", "objectIdentification")
     ds2 = ds2.withColumnRenamed("_c1", "description")
     ds2 = ds2.withColumnRenamed("_c2", "x")
     ds2 = ds2.withColumnRenamed("_c3", "y")
 
-    ds.printSchema()
-    ds2.printSchema()
+//    ds.printSchema()
+//    ds2.printSchema()
 
     ds.createOrReplaceTempView("trajectory")
 
     ds2.createOrReplaceTempView("poi")
 
     simba.indexTable("trajectory", RTreeType, "trajectoriesIndex",  Array("x", "y"))
-    simba.showIndex("trajectory") 
+    val result = ds.toDF().mapPartitions(partition =>
+      Iterator(mbrs(ListBuffer()).addMbr(partition))).reduce((x,y) => x.merge(y))
+//    println(result.mbrList.size)
+//    simba.showIndex("trajectory")
 //    simba.sql("SELECT INDEX trajectoriesIndex")
 //    simba.indexTable("poi", RTreeType, "poisIndex",  Array("x", "y") )
-//    simba.sql("SELECT INDEX poisIndex")
 
     //    simba.loadIndex("poisIndex", "/home/pgiorgianni/Downloads/POIsIndex")
 //    simba.persistIndex("poisIndex", "/home/pgiorgianni/Downloads/POIsIndex")
@@ -124,11 +176,66 @@ object Project {
   def question5(simba : SimbaSession) : Unit ={
     import simba.simbaImplicits._
     //gets the values of each dataset and does some filtering for weekdays
-    var poi = simba.sql("Select x as poix, y as poiy from poi")
-    var trajectories = simba.sql("Select * from trajectory where where date_format(timeRead, 'u') <= 5")
+    var poi = simba.sql("Select x as poix, y as poiy, objectIdentification as id from poi")
+    var trajectories = simba.sql("Select x, y, objectIdentification, date_format(timeRead, 'M') as Month," +
+      " date_format(timeRead, 'y') as year from trajectory where where date_format(timeRead, 'u') <= 5 and " +
+      "(date_format(timeRead, 'y') = 2008 or date_format(timeRead, 'y') = 2009)")
     //joins the sets where they are x distance appart then groups them by point and sorts them to count
-    var solution = poi.distanceJoin(trajectories, Array("poix", "poiy"), Array("x", "y"), 100).groupBy("poix", "poiy").count().sort(desc("count")).limit(20)
-    solution.show(20)
+    var solution = poi.distanceJoin(trajectories, Array("poix", "poiy"), Array("x", "y"), 100).select("id",
+      "Month", "year").distinct().cache()//.groupBy("poix", "poiy").count().sort(desc("count")).limit(20)
+    var twoThousandAnd8 = solution.where("year = 2008").groupBy("id", "month").count().sort(desc("count")).select("id").cache()
+    println("January 2008")
+    twoThousandAnd8.where("month = 1").show(10)
+    println("February 2008")
+    twoThousandAnd8.where("month = 2").show(10)
+    println("March 2008")
+    twoThousandAnd8.where("month = 3").show(10)
+    println("April 2008")
+    twoThousandAnd8.where("month = 4").show(10)
+    print("May 2008")
+    twoThousandAnd8.where("month = 5").show(10)
+    println("June 2008")
+    twoThousandAnd8.where("month = 6").show(10)
+    println("July 2008")
+    twoThousandAnd8.where("month = 7").show(10)
+    println("August 2008")
+    twoThousandAnd8.where("month = 8").show(10)
+    println("September 2008")
+    twoThousandAnd8.where("month = 9").show(10)
+    println("October 2008")
+    twoThousandAnd8.where("month = 10").show(10)
+    println("November 2008")
+    twoThousandAnd8.where("month = 11").show(10)
+    println("December 2008")
+    twoThousandAnd8.where("month = 12").show(10)
+
+    var twoThousandAnd9 = solution.where("year = 2009").groupBy("id", "month").count().sort(desc("count")).select("id").cache()
+    println("January 2009")
+    twoThousandAnd9.where("month = 1").show(10)
+    println("February 2009")
+    twoThousandAnd9.where("month = 2").show(10)
+    println("March 2009")
+    twoThousandAnd9.where("month = 3").show(10)
+    println("April 2009")
+    twoThousandAnd9.where("month = 4").show(10)
+    print("May 2009")
+    twoThousandAnd9.where("month = 5").show(10)
+    println("June 2009")
+    twoThousandAnd9.where("month = 6").show(10)
+    println("July 2009")
+    twoThousandAnd9.where("month = 7").show(10)
+    println("August 2009")
+    twoThousandAnd9.where("month = 8").show(10)
+    println("September 2009")
+    twoThousandAnd9.where("month = 9").show(10)
+    println("October 2009")
+    twoThousandAnd9.where("month = 10").show(10)
+    println("November 2009")
+    twoThousandAnd9.where("month = 11").show(10)
+    println("December 2009")
+    twoThousandAnd9.where("month = 12").show(10)
+
+    //    solution.show(20)
   }
 
 }
